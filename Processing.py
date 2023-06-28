45b919cebf47ec436d3032ef8edd995502280408
Avadhut Patil <96344902+AP-inventory@users.noreply.github.com>
2023-06-28 12:45:21 +0530

    Update Processing.py

--- Processing.py
+++ Processing.py
@@ -1,53 +1,106 @@
-# Required Libraries:
-# - spaCy (for NLP processing)
-# - numpy (for numerical operations)
-# - random (for random selection)
+from PIL import Image
 
-import spacy
-import numpy as np
-import random
+# Load the image
+image = Image.open("input_image.jpg")
 
-# Load the spaCy English model
-nlp = spacy.load('en_core_web_sm')
+# Resize the image to a specific width and height
+width, height = 800, 600
+resized_image = image.resize((width, height))
 
-# Define a list of sample motions
-motions = {
-    'head': [
-        'Nod',
-        'Shake',
-        'Tilt',
-        'Turn'
-    ],
-    'lip': [
-        'Open',
-        'Close',
-        'Smile',
-        'Pucker',
-        'Kiss'
-    ]
-}
+# Save the resized image
+resized_image.save("preprocessed_image.jpg")
 
-# Function to generate motion based on text input
-def generate_motion(text):
-    # Process the text using spaCy
-    doc = nlp(text)
-    
-    # Get the verb from the text
-    verb = [token.lemma_ for token in doc if token.pos_ == 'VERB']
-    
-    # Choose a random head motion from the available list
-    head_motion = random.choice(motions['head'])
-    
-    # Choose a random lip motion from the available list
-    lip_motion = random.choice(motions['lip'])
-    
-    # Generate the final motion text
-    final_motion = f'{verb[0]} with {head_motion} head and {lip_motion} lips'
-    
-    return final_motion
 
-# Example usage
-input_text = "I want to speak"
-motion_output = generate_motion(input_text)
-print(motion_output)
-    
+
+
+from PIL import Image
+
+# Load the image
+image = Image.open("input_image.jpg")
+
+# Define the cropping region (coordinates: left, upper, right, lower)
+cropping_coords = (100, 100, 500, 400)
+
+# Crop the image
+cropped_image = image.crop(cropping_coords)
+
+# Save the cropped image
+cropped_image.save("preprocessed_image.jpg")
+
+
+
+
+from PIL import ImageEnhance
+
+# Load the image
+image = Image.open("input_image.jpg")
+
+# Enhance the image by adjusting brightness and contrast
+enhancer = ImageEnhance.Brightness(image)
+enhanced_image = enhancer.enhance(1.5)  # Increase brightness by a factor of 1.5
+
+enhancer = ImageEnhance.Contrast(enhanced_image)
+enhanced_image = enhancer.enhance(1.2)  # Increase contrast by a factor of 1.2
+
+# Save the enhanced image
+enhanced_image.save("preprocessed_image.jpg")
+
+
+
+
+pip install dlib pyttsx3
+
+
+import cv2
+import dlib
+import pyttsx3
+
+# Load the shape predictor model
+shape_predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+# Initialize the text-to-speech engine
+engine = pyttsx3.init()
+
+# Define the image and text
+image_path = "avatar_image.jpg"
+text_input = "Hello, how are you?"
+
+# Load the image
+image = cv2.imread(image_path)
+
+# Convert the image to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Detect facial landmarks
+detector = dlib.get_frontal_face_detector()
+faces = detector(gray)
+if len(faces) == 0:
+    print("No faces found in the image.")
+    exit()
+face = faces[0]
+landmarks = shape_predictor(gray, face)
+
+# Get the mouth region landmarks (change the indices according to your specific model)
+mouth_landmarks = landmarks.parts()[48:68]
+
+# Convert the text to speech
+engine.save_to_file(text_input, "speech_output.wav")
+engine.runAndWait()
+
+# Play the generated speech audio
+# You'll need to install a suitable audio player and provide its path
+audio_player = "/path/to/audio/player"
+audio_file = "speech_output.wav"
+cv2.waitKey(1000)  # Wait for 1 second before playing the audio (adjust as needed)
+cv2.destroyAllWindows()
+import subprocess
+subprocess.run([audio_player, audio_file])
+
+# Animate the mouth based on the speech
+for landmark in mouth_landmarks:
+    cv2.circle(image, (landmark.x, landmark.y), 2, (0, 0, 255), -1)
+
+# Display the animated image
+cv2.imshow("Animated Image", image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
