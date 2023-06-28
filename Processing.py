84d848e7f2f8b08b4f126fd1595db0b5c3083b26
Avadhut Patil <96344902+AP-inventory@users.noreply.github.com>
2023-06-28 12:40:50 +0530

    Update Processing.py

--- Processing.py
+++ Processing.py
@@ -11,13 +11,21 @@ import random
 nlp = spacy.load('en_core_web_sm')
 
 # Define a list of sample motions
-motions = [
-    'Walk forward',
-    'Jump',
-    'Wave',
-    'Spin around',
-    'Clap hands'
-]
+motions = {
+    'head': [
+        'Nod',
+        'Shake',
+        'Tilt',
+        'Turn'
+    ],
+    'lip': [
+        'Open',
+        'Close',
+        'Smile',
+        'Pucker',
+        'Kiss'
+    ]
+}
 
 # Function to generate motion based on text input
 def generate_motion(text):
@@ -27,15 +35,19 @@ def generate_motion(text):
     # Get the verb from the text
     verb = [token.lemma_ for token in doc if token.pos_ == 'VERB']
     
-    # Choose a random motion from the available list
-    motion = random.choice(motions)
+    # Choose a random head motion from the available list
+    head_motion = random.choice(motions['head'])
+    
+    # Choose a random lip motion from the available list
+    lip_motion = random.choice(motions['lip'])
     
     # Generate the final motion text
-    final_motion = f'{verb[0]} and {motion}'
+    final_motion = f'{verb[0]} with {head_motion} head and {lip_motion} lips'
     
     return final_motion
 
 # Example usage
-input_text = "I want to dance"
+input_text = "I want to speak"
 motion_output = generate_motion(input_text)
 print(motion_output)
+    
